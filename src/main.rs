af41096a752a71cc9bc8ab24355f8d7299c7671c
Hildeberto Mendonca <me@hildeberto.com>
2025-12-27 15:27:54 -0500

    Added support for logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use env_logger::Env;
 use minimily::config::load_config;
 
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = load_config().expect("Failed to load config");
-    println!("Port: {}", config.server_port);
+    log::info!("Port: {}", config.server_port);
 }
