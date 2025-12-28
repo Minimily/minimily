170d4d33416b7523ea66c34d76431cf9dd303310
Hildeberto Mendonca <me@hildeberto.com>
2025-12-27 23:24:10 -0500

    Creating a PostgreSQL connection pool

--- src/config.rs
+++ src/config.rs
@@ -5,15 +5,23 @@ use std::io::Error;
 #[derive(serde::Deserialize)]
 pub struct Config {
     pub server_port: u16,
+    pub database: DatabaseConfig,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseConfig {
+    pub url: String,
 }
 
 pub fn load_config() -> Result<Config, Error> {
     dotenv().ok();
 
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string()).parse().expect("Configuration: PORT must be a number");
+    let database_url = env::var("DATABASE_URL").unwrap_or_else(|_| "".to_string());
 
     let config: Config = Config {
         server_port: port,
+        database: DatabaseConfig { url: database_url },
     };
 
     Ok(config)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use env_logger::Env;
-use minimily::config::load_config;
+use sqlx::PgPool;
+use minimily::config::{load_config, Config};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let config = load_config().expect("Failed to load config");
+    let _pool = match get_pool(&config).await {
+        Ok(pool) => pool,
+        Err(e) => {
+            log::error!("Error connecting to database: {}", e);
+            return;
+        }
+    };
+
     log::info!("Port: {}", config.server_port);
+}
+
+async fn get_pool(config: &Config) -> Result<PgPool, sqlx::Error> {
+    match PgPool::connect(&config.database.url).await {
+        Ok(pool) => Ok(pool),
+        Err(e) => Err(e)
+    }
 }
